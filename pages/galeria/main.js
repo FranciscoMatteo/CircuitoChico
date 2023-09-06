dfaabf6c63817849a4cf7eb5a996a6c611be73b9
Francisco Matteo <fams_23@hotmail.com>
2023-09-06 03:55:30 -0300

    Creando galeria

---
+++ pages/galeria/main.js
@@ -0,0 +1,12 @@
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg");
+
+function openFulImg(reference) {
+    fulImgBox.style.display= "flex";
+    fulImg.src = reference;
+}
+
+
+function closeImg() {
+    fulImgBox.style.display = "none";
+}
