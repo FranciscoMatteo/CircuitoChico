56e72474a01f8e7833f3f32e7a4833152f9c6778
Lautaro Ledesma <lautaroledesma_1@hotmail.com>
2023-10-07 05:08:18 -0300

    Se hizo responsive el index y liga ademas se modificaron animaciones con CSS

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-function mostrarLogo() {
-  const header = document.getElementById("boxHeader");
-  const logo = header.querySelector(".logo");
-  const menu = header.querySelector(".navbar");
-
-  // Agrega clases para mostrar el logo y el menú
-  header.classList.add("mostrar");
-  logo.style.display = "flex";
-  menu.classList.add("mostrar-menu");
-}
 let tamanoDeseado = 1366;
 let aca = 0;
 window.addEventListener("scroll", () => {
@@ -67,6 +57,24 @@ window.addEventListener("scroll", () => {
       menu.classList.add("sacar-menu");
       box.style.opacity = "1";
     }
+  }
+});
+document.addEventListener("scroll", function () {
+  let boton = document.getElementById("insBoton");
+  let posicionActual = window.scrollY; // Obtiene la posición vertical actual del scroll
+  let alturaPagina = document.body.scrollHeight - window.innerHeight - 500;
+  let alturaPagina2 = document.body.scrollHeight - window.innerHeight - 400; // Calcula la altura total de la página
+
+  // Verifica si estás al final de la página
+  if (posicionActual >= alturaPagina) {
+    boton.style.opacity = "0"; // Oculta el botón cuando estás al final
+  } else {
+    boton.style.opacity = "1"; // Muestra el botón en cualquier otro lugar de la página
+  }
+  if (posicionActual >= alturaPagina2) {
+    boton.style.display = "none"; // Oculta el botón cuando estás al final
+  } else {
+    boton.style.display = ""; // Muestra el botón en cualquier otro lugar de la página
   }
 });
 
