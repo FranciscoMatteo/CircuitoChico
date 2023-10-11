5fe2a82ccb1f30f945a05a70a42ce0b899245b9f
Francisco Matteo <fams_23@hotmail.com>
2023-10-11 17:15:42 -0300

    Solucionando errores

--- insBoton.js
+++ insBoton.js
@@ -1,30 +1,24 @@
-const fulImgBox = document.getElementById("fulImgBox"),
-  fulImg = document.getElementById("fulImg");
-
-function openFulImg(reference) {
-  fulImgBox.style.display = "flex";
-  fulImg.src = reference;
-}
-
-function closeImg() {
-  fulImgBox.style.display = "none";
-}
-
 document.addEventListener("scroll", function () {
   let boton = document.getElementById("insBoton");
   let posicionActual = window.scrollY; // Obtiene la posición vertical actual del scroll
   let alturaPagina = document.body.scrollHeight - window.innerHeight - 500;
   let alturaPagina2 = document.body.scrollHeight - window.innerHeight - 400; // Calcula la altura total de la página
+  let anchoPagina = window.innerWidth;
 
   // Verifica si estás al final de la página
-  if (posicionActual >= alturaPagina) {
-    boton.style.opacity = "0";
+  if (anchoPagina <= 1000) {
+    if (posicionActual >= alturaPagina) {
+      boton.style.opacity = "0";
+    } else {
+      boton.style.opacity = "1";
+    }
+    if (posicionActual >= alturaPagina2) {
+      boton.style.display = "none";
+    } else {
+      boton.style.display = "";
+    }
   } else {
     boton.style.opacity = "1";
-  }
-  if (posicionActual >= alturaPagina2) {
-    boton.style.display = "none";
-  } else {
     boton.style.display = "";
   }
 });
--- main.js
+++ main.js
@@ -1,13 +1,5 @@
-function mostrarLogo() {
-  const header = document.getElementById("boxHeader");
-  const logo = header.querySelector(".logo");
-  const menu = header.querySelector(".navbar");
-
-  // Agrega clases para mostrar el logo y el menú
-  header.classList.add("mostrar");
-  logo.style.display = "flex";
-  menu.classList.add("mostrar-menu");
-}
+let anchoPagina = window.innerWidth;
+let tamanoDeseado = 1366;
 let aca = 0;
 window.addEventListener("scroll", () => {
   const logo = document.getElementById("boxLogo");
@@ -36,18 +28,63 @@ window.addEventListener("scroll", () => {
     menu.classList.remove("mostrar-menu");
   }
 
-  if (scrollY > 500) {
-    if (aca > 3) {
-      aca -= 1;
+  if (anchoPagina <= 768) {
+    box.style.display = "none";
+  } else {
+    if (window.innerWidth <= tamanoDeseado) {
+      if (scrollY > 400) {
+        if (aca > 3) {
+          aca -= 1;
+        } else {
+          aca += 1;
+        }
+        mostrarLogo();
+        box.style.opacity = "0";
+        menu.classList.remove("sacar-menu");
+      } else if (aca > 1) {
+        sacarLogo();
+        menu.classList.add("sacar-menu");
+        box.style.opacity = "1";
+      }
     } else {
-      aca += 1;
+      if (scrollY > 650) {
+        if (aca > 3) {
+          aca -= 1;
+        } else {
+          aca += 1;
+        }
+        mostrarLogo();
+        box.style.opacity = "0";
+        menu.classList.remove("sacar-menu");
+      } else if (aca > 1) {
+        sacarLogo();
+        menu.classList.add("sacar-menu");
+        box.style.opacity = "1";
+      }
     }
-    mostrarLogo();
-    box.style.opacity = "0";
-    menu.classList.remove("sacar-menu");
-  } else if (aca > 1) {
-    sacarLogo();
-    menu.classList.add("sacar-menu");
-    box.style.opacity = "1";
   }
 });
+document.addEventListener("scroll", function () {
+  let boton = document.getElementById("insBoton");
+  let posicionActual = window.scrollY; // Obtiene la posición vertical actual del scroll
+  let alturaPagina = document.body.scrollHeight - window.innerHeight - 500;
+  let alturaPagina2 = document.body.scrollHeight - window.innerHeight - 400; // Calcula la altura total de la página
+
+  // Verifica si estás al final de la página
+  if (posicionActual >= alturaPagina) {
+    boton.style.opacity = "0"; // Oculta el botón cuando estás al final
+  } else {
+    boton.style.opacity = "1"; // Muestra el botón en cualquier otro lugar de la página
+  }
+  if (posicionActual >= alturaPagina2) {
+    boton.style.display = "none"; // Oculta el botón cuando estás al final
+  } else {
+    boton.style.display = ""; // Muestra el botón en cualquier otro lugar de la página
+  }
+});
+
+//  Agregar un event listener para el evento "resize" de la ventana
+// window.addEventListener("resize", verificarTamañoPantalla);
+
+//   Llamar a la función una vez al cargar la página para verificar el tamaño inicial
+// verificarTamañoPantalla();
